9a052f6fac06f8298d3b57268dd49d5d82e7354f
giusmili <giusmili67@gmail.com>
2026-01-16 14:35:46 +0100

    html css

---
+++ eslint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    ignores: ["node_modules/**", ".dist/**", "dist/**"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        console: "readonly",
+        Audio: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        requestAnimationFrame: "readonly",
+        cancelAnimationFrame: "readonly",
+      },
+    },
+    rules: {},
+  },
+];
